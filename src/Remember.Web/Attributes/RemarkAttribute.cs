409882208f83cb6de0d591df13675ec13599a0fa
yiyun <yiyungent@gmail.com>
2019-03-11 19:49:26 +0800

    放弃做安装进度条了

---
+++ src/Remember.Web/Attributes/RemarkAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Remember.Web.Attributes
+{
+    /// <summary>
+    /// 备注特性
+    /// </summary>
+    public class RemarkAttribute : Attribute
+    {
+        public string Remark { get; set; }
+
+        public RemarkAttribute(string remark)
+        {
+            this.Remark = remark;
+        }
+    }
+}
--- src/Remember.Web/Controllers/InstallController.cs
+++ src/Remember.Web/Controllers/InstallController.cs
@@ -6,12 +6,25 @@ using System.Web.Mvc;
 
 using Castle.ActiveRecord;
 using Newtonsoft.Json;
+using Remember.Web.Models;
+using System.Threading;
 
 namespace Remember.Web.Controllers
 {
+    public delegate void Del_Install(ref InstallProgressList list);
     public class InstallController : Controller
     {
-        public static Models.CurrentProgress CurrentProgress { get; set; }
+        private event Del_Install _onInstall;
+
+        private static InstallProgressList _installProgressList = new InstallProgressList();
+
+        private bool _isInstallPause = false;
+
+        public InstallController()
+        {
+            _onInstall += CreateSchema;
+            _onInstall += InitTableData;
+        }
 
         #region 安装视图
         [HttpGet]
@@ -21,100 +34,79 @@ namespace Remember.Web.Controllers
         }
         #endregion
 
-        #region 执行安装
+        #region 开始安装
         [HttpPost]
         public ActionResult Index(string flag)
         {
             if (Request.IsAjaxRequest())
             {
-                Models.InstallInfoViewModel model = new Models.InstallInfoViewModel
-                {
-                    InstallTime = DateTime.Now,
-                    Errors = new List<Exception>()
-                };
+                ExecInstall();
 
-                bool isContinue = true;
-
-                try
-                {
-                    CurrentProgress = new Models.CurrentProgress();
-                    System.Threading.Thread.Sleep(1000);
-                    CurrentProgress.currentInfo = "开始创建数据库表结构...";
-                    CurrentProgress.currentPos = 10;
-                    CurrentProgress.code = 1;
-                    CreateSchema();
-                    System.Threading.Thread.Sleep(1000);
-                    CurrentProgress.currentInfo = "数据库表结构创建成功";
-                    CurrentProgress.currentPos += 10;
-                }
-                catch (Exception ex)
-                {
-                    CurrentProgress.currentInfo = "数据库表结构创建失败";
-                    CurrentProgress.code = -2;
-                    model.Errors.Add(ex);
-                    isContinue = false;
-                }
-
-                if (isContinue)
-                {
-                    try
-                    {
-                        // 若数据库表结构创建成功，才继续进行初始化表数据
-                        System.Threading.Thread.Sleep(1000);
-                        CurrentProgress.currentInfo = "开始表初始化数据...";
-                        CurrentProgress.currentPos += 10;
-                        InitTableData();
-                        System.Threading.Thread.Sleep(1000);
-                        CurrentProgress.currentInfo = "表初始化数据完成";
-                    }
-                    catch (Exception ex)
-                    {
-                        CurrentProgress.currentInfo = "数据表初始化数据失败";
-                        CurrentProgress.code = -2;
-                        model.Errors.Add(ex);
-                    }
-                }
-
-                if (model.Errors.Count == 0)
-                {
-                    model.Result = Models.InstallResult.Success;
-                }
-                else
-                {
-                    model.Result = Models.InstallResult.Failure;
-                }
-
-                return PartialView("_InstallProgress", model);
+                return PartialView("_InstallProgress");
             }
             else
             {
                 return Content("非法请求");
             }
         }
+        #endregion
+
+        #region 执行安装
+        public void ExecInstall()
+        {
+            _onInstall(ref _installProgressList);
+        }
+        #endregion
 
         #region 初始化表数据
-        private void InitTableData()
+        private void InitTableData(ref InstallProgressList list)
         {
+            InstallProgress pro = new InstallProgress { info = "表初始化数据" };
+            try
+            {
 
+                pro.isSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                pro.exception = ex;
+            }
+            Thread.Sleep(3000);
         }
         #endregion
 
         #region 创建数据库表结构
-        private void CreateSchema()
+        private void CreateSchema(ref InstallProgressList list)
         {
-            ActiveRecordStarter.CreateSchema();
+            InstallProgress pro = new InstallProgress { info = "创建数据库 表结构" };
+            list.AddItem(pro);
+            try
+            {
+                ActiveRecordStarter.CreateSchema();
+                pro.isSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                pro.exception = ex;
+            }
+            Thread.Sleep(3000);
         }
         #endregion
 
-        #endregion
-
-        #region 获取当前安装进度
+        #region 获取安装进度
         [HttpPost]
         public JsonResult GetProgress()
         {
-            return Json(CurrentProgress);
+            var progressInfo = new { progress = _installProgressList.List, code = _isInstallPause ? -1 : 1 };
+            return Json(progressInfo);
         }
         #endregion
 
+        #region 获取安装步骤数
+        public int GetProgressCount()
+        {
+            return _onInstall.GetInvocationList().Count();
+        }
+        #endregion
     }
 }
