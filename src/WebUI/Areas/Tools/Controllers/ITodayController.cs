4d220c3a6435bf5d9cc3b99e669478705db590d5
yiyun <yiyungent@gmail.com>
2019-08-10 15:56:39 +0800

    update

---
+++ src/WebUI/Areas/Tools/Controllers/ITodayController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Areas.Tools.Controllers
+{
+    /// <summary>
+    /// 工具箱-爱今天
+    /// </summary>
+    public class ITodayController : Controller
+    {
+        #region 首页-展示可视化数据
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 导入数据
+        [HttpPost]
+        public JsonResult Import()
+        {
+            return Json(new { });
+        }
+        #endregion
+    }
+}
