819a2c5388ec9c7581dff3869174b8f174c4094b
yiyun <yiyungent@gmail.com>
2019-08-12 00:16:43 +0800

    add Card pages

---
+++ src/Domain/Comment.cs
@@ -0,0 +1,76 @@
+﻿using Castle.ActiveRecord;
+using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    [ActiveRecord]
+    public class Comment : BaseEntity<Comment>
+    {
+        /// <summary>
+        /// 内容
+        /// </summary>
+        [Display(Name = "内容")]
+        [Property(Length = 2200, NotNull = false)]
+        public string Content { get; set; }
+
+        #region Relationships
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [Display(Name = "创建时间")]
+        [Property]
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 最近更新
+        /// </summary>
+        [Display(Name = "最近更新")]
+        [Property]
+        public DateTime LastUpdateTime { get; set; }
+
+        /// <summary>
+        /// 作者
+        /// </summary>
+        [Display(Name = "作者")]
+        [BelongsTo(Column = "AuthorId", NotNull = true)]
+        public UserInfo Author { get; set; }
+
+        /// <summary>
+        /// 回复评论（父级评论）
+        /// </summary>
+        [BelongsTo(Column = "ReplyCommentId", NotNull = false)]
+        public Comment ReplyComment { get; set; }
+
+        /// <summary>
+        /// 评论类型
+        /// </summary>
+        [Property]
+        public CommentType? CommentType { get; set; }
+
+        /// <summary>
+        /// 评论对象ID
+        ///     例如评论某卡片，即是此被评论卡片的ID
+        /// </summary>
+        [Property(NotNull = true)]
+        public int CommentTargetId { get; set; }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// 评论类型（属于在何处的评论）
+    /// </summary>
+    public enum CommentType
+    {
+        CardInfo = 1,
+        CardBox = 2
+    }
+}
--- src/WebUI/Controllers/CardBoxController.cs
+++ src/WebUI/Controllers/CardBoxController.cs
@@ -1,13 +1,31 @@
-﻿using System;
+﻿using Core;
+using Domain;
+using Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUI.Areas.Admin.Models.CardInfoVM;
 
 namespace WebUI.Controllers
 {
     public class CardBoxController : Controller
     {
+        #region Fields
+        private CardInfoService _cardInfoService;
+
+        private CardBoxService _cardBoxService;
+        #endregion
+
+        #region Ctor
+        public CardBoxController()
+        {
+            this._cardInfoService = Container.Instance.Resolve<CardInfoService>();
+            this._cardBoxService = Container.Instance.Resolve<CardBoxService>();
+        }
+        #endregion
+
         #region 卡片盒市场首页
         /// <summary>
         /// 卡片盒市场首页
@@ -39,6 +57,20 @@ namespace WebUI.Controllers
         /// <param name="id">卡片Id</param>
         /// <returns></returns>
         public ActionResult Card(int id = 0, string q = null)
+        {
+            CardInfoViewModel viewModel = null;
+            if (_cardInfoService.Exist(id))
+            {
+                CardInfo dbModel = _cardInfoService.GetEntity(id);
+                viewModel = (CardInfoViewModel)dbModel;
+            }
+
+            return View(viewModel);
+        }
+        #endregion
+
+        #region 评论编辑页
+        public ActionResult Comment()
         {
             return View();
         }
