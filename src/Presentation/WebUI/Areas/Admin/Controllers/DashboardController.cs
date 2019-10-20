b0c681c679cd097eac223ae2ce6770c9a7202976
yiyun <yiyungent@gmail.com>
2019-10-21 01:18:28 +0800

    added: 添加仪表盘页面初步

--- src/Presentation/WebUI/Areas/Admin/Controllers/DashboardController.cs
+++ src/Presentation/WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -1,5 +1,6 @@
 ﻿using Core;
 using Domain;
+using Services.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +17,34 @@ namespace WebUI.Areas.Admin.Controllers
     /// </summary>
     public class DashboardController : Controller
     {
+        #region Fields
+        private readonly ILogInfoService _logInfoService;
+        #endregion
+
         #region Ctor
-        public DashboardController()
+        public DashboardController(ILogInfoService logInfoService)
         {
-           
+            this._logInfoService = logInfoService;
+        }
+        #endregion
+
+        #region 仪表盘1
+        public ViewResult Index()
+        {
+            DashboardOneViewModel viewModel = new DashboardOneViewModel();
+            viewModel.PV = 1;
+            viewModel.UV = 2;
+            viewModel.JumpRate = 23;
+            viewModel.NewUserReg = 213;
+
+            return View(viewModel);
+        }
+        #endregion
+
+        #region 仪表盘2
+        public ViewResult IndexTwo()
+        {
+            return View();
         }
         #endregion
 
---
+++ src/Presentation/WebUI/Areas/Admin/Models/DashboardVM/DashboardOneViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Areas.Admin.Models.DashboardVM
+{
+    public class DashboardOneViewModel
+    {
+        /// <summary>
+        /// 独立访客量
+        /// </summary>
+        public int UV { get; set; }
+
+        /// <summary>
+        /// 访问量
+        /// </summary>
+        public int PV { get; set; }
+
+        /// <summary>
+        /// 跳出率
+        /// </summary>
+        public int JumpRate { get; set; }
+
+        /// <summary>
+        /// 新用户注册量
+        /// 今日新注册量
+        /// </summary>
+        public int NewUserReg { get; set; }
+
+    }
+}
