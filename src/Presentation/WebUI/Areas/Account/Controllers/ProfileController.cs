c32226672dea5901a94f96b05447c6209afed913
yiyun <yiyungent@gmail.com>
2019-12-30 20:03:43 +0800

    fixed: 个人中心时间轴

--- src/Presentation/WebUI/Areas/Account/Controllers/ProfileController.cs
+++ src/Presentation/WebUI/Areas/Account/Controllers/ProfileController.cs
@@ -22,10 +22,11 @@ namespace WebUI.Areas.Account.Controllers
     {
         #region Fields
         private IUserInfoService _userInfoService;
+        private readonly IArticleService _articleService;
         #endregion
 
         #region Ctor
-        public ProfileController(IUserInfoService userInfoService)
+        public ProfileController(IUserInfoService userInfoService, IArticleService articleService)
         {
             ViewBag.PageHeader = "个人中心";
             ViewBag.PageHeaderDescription = "";
@@ -35,11 +36,12 @@ namespace WebUI.Areas.Account.Controllers
             };
 
             this._userInfoService = userInfoService;
+            this._articleService = articleService;
         }
         #endregion
 
         #region 个人中心首页
-        public ActionResult Index(string userName = null)
+        public ActionResult Index(string userName = null, int pageIndex = 1)
         {
             if (userName == null)
             {
@@ -47,13 +49,59 @@ namespace WebUI.Areas.Account.Controllers
             }
             // 此主页对应的UserInfo
             UserInfo viewModel = AccountManager.GetUserInfoByUserName(userName);
+            int authorId = viewModel.ID;
             if (viewModel == null)
             {
                 // 不存在此用户
                 return new View_NotExistAccountResult();
             }
 
+            int pageSize = 6;
+            pageIndex = pageIndex >= 1 ? pageIndex : 1;
+            Query(pageIndex, pageSize, out IList<Article> list, out int totalCount, authorId);
+
+            ListViewModel<Article> articles = new ListViewModel<Article>(list, pageIndex: pageIndex, pageSize: pageSize, totalCount: totalCount);
+            ViewBag.ArticleVM = articles;
+
             return View(viewModel);
+        }
+
+        private void Query(int pageIndex, int pageSize, out IList<Article> list, out int totalCount, int authorId)
+        {
+            // 输入的查询关键词
+            string query = Request["q"]?.Trim() ?? "";
+            // 查询类型
+            QueryType queryType = new QueryType();
+            queryType.Val = Request["type"]?.Trim() ?? "title";
+            switch (queryType.Val.ToLower())
+            {
+                case "title":
+                    queryType.Text = "标题";
+                    list = this._articleService.Filter<int>(pageIndex, pageSize, out totalCount, m => m.Title.Contains(query) && m.AuthorId == authorId && !m.IsDeleted, m => m.ID, false).ToList();
+                    break;
+                case "id":
+                    queryType.Text = "ID";
+                    if (int.TryParse(query, out int queryId))
+                    {
+                        list = this._articleService.Filter<int>(pageIndex, pageSize, out totalCount, m => m.ID == queryId && m.AuthorId == authorId && !m.IsDeleted, m => m.ID, false).ToList();
+                    }
+                    else if (string.IsNullOrEmpty(query))
+                    {
+                        list = this._articleService.Filter<int>(pageIndex, pageSize, out totalCount, m => m.AuthorId == authorId && !m.IsDeleted, m => m.ID, false).ToList();
+                    }
+                    else
+                    {
+                        list = new List<Article>();
+                        totalCount = 0;
+                    }
+                    break;
+                default:
+                    queryType.Text = "标题";
+                    list = this._articleService.Filter<int>(pageIndex, pageSize, out totalCount, m => m.Title.Contains(query) && m.AuthorId == authorId && !m.IsDeleted, m => m.ID, false).ToList();
+                    break;
+            }
+            ViewBag.Query = query;
+            ViewBag.QueryType = queryType;
         }
         #endregion
 
